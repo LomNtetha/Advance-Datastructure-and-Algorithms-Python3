3933cc52bb76ef092bf6e30e76f20bb3eb479ddc
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-01-03 13:12:15 +0200

    update my playground

--- practice.py
+++ practice.py
@@ -1,19 +1,25 @@
-def maxmum_activities(start,end):
+def max_activities(start,end):
 
     activities = list(zip(start,end))
-    print(activities)
 
-    activities.sort(key=lambda x :x[1])
-
-    print(f"sorted: {activities}")
-
+    activities.sort(key=lambda x:x[1])
 
     last_end = activities[0][1]
     count = 1
     selected_activities = [0]
 
+    for i in range(1, len(activities)):
+
+        if activities[i][0]>= last_end:
+            count+=1
+            selected_activities.append(i)
+            last_end = activities[i][1]
+
+    return count, selected_activities
 start = [1, 3, 0, 5, 8, 5]
 end = [2, 4, 6, 7, 9, 9]
 
+count, selected = max_activities(start, end)
 
-act = maxmum_activities(start, end)
+print(f"Number of activities {count}")
+print(f"slected activities {selected}")
