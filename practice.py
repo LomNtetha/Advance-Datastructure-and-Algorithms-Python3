e004141a602113e729a0d2c6136121db158bf56c
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-01-04 09:26:04 +0200

    update my playground

--- practice.py
+++ practice.py
@@ -27,32 +27,28 @@ count, selected = max_activities(start, end)
 print(f"Number of activities {count}")
 print(f"slected activities {selected}")
 
-
-def fraction_capacity(weights,values,capacity):
+def fractional_kanapsack(weights,values,capacity):
 
     zip_items = zip(values,weights)
+    zip_items_fractions = ((values/weights,weights)for values,weights in zip_items)
 
-    zip_fractions_items = ((v/w,w) for v, w in zip_items)
-
-    items = sorted(zip_fractions_items, reverse=True)
+    items = sorted(zip_items_fractions, reverse= True)
 
     total_value = 0.0
 
-    for items_per_value,weight in items:
-
+    for item_per_value, weight in items:
         if capacity >= weight:
-            total_value+=items_per_value*weight
-            capacity -= weight
+            total_value += item_per_value * weight
+            capacity-=weight
         else:
-            total_value+= items_per_value * capacity
+            total_value += item_per_value*capacity
             break
-
     return total_value
-
 
 weights = [10, 20, 30]  # Weights of items
 values = [60, 100, 120]  # Values of items
 capacity = 50  # Capacity of the knapsack
-total = fraction_capacity(weights,values,capacity)
 
-print(f"Here is the return value {total}")
+total_v = fractional_kanapsack(weights,values,capacity)
+
+print(f"here is the Total Value: {total_v}")
