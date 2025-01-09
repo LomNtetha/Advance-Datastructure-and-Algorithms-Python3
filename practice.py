4096180149849f1554a07c8544725e2f69c69aa6
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-01-09 09:16:11 +0200

    update my playground

--- practice.py
+++ practice.py
@@ -20,11 +20,12 @@ c, n = max_denominations(denominations, amount)
 print(f"here is the coin used: {c}")
 print (f"here is the number of coins {n}")
 
-def maximum_activities(start,end):
+
+def max_activities(start,end):
+
     activities = list(zip(start,end))
 
     activities.sort(key=lambda x:x[1])
-   
 
     selected_activities = [0]
     last_end_time = activities[0][1]
@@ -32,16 +33,14 @@ def maximum_activities(start,end):
 
     for i in range(1, len(activities)):
         if activities[i][0] >= last_end_time:
-            count += 1
-            selected_activities.append(i) 
+            selected_activities.append(i)
+            count+=1
             last_end_time = activities[i][1]
-    return selected_activities,count
-    
-
+    return count, selected_activities
 start = [1, 3, 0, 5, 8, 5]
 end = [2, 4, 6, 7, 9, 9]
 
-s,e = maximum_activities(start,end)
+s,e = max_activities(start,end)
+print(s)
+print(e)
 
-print (s)
-print (e)
