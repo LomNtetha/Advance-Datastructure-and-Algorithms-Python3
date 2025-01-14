566e032ad65d2734bf67e126cb0f371196b34b49
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-01-14 17:09:18 +0200

    update the playground

--- practice.py
+++ practice.py
@@ -192,3 +192,44 @@ k = 3
 min_diff = Minimize_difference(heights,k)
 
 print(min_diff)
+
+
+def dijkstra_algorithms(graph,source):
+    distances = {i: float('inf') for i in graph}
+    distances[source] = 0
+
+    min_heap = [(0,source)] 
+
+    while min_heap:
+        current_distance, current_node = heapq.heappop(min_heap)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor,weight in graph[current_node]:
+            dist = current_distance + weight
+
+            if dist < distances[neighbor]:
+                distances[neighbor] = dist
+
+                heapq.heappush(min_heap, (dist,neighbor))
+
+    return [distances[i] for i in range(len(graph))]
+graph = {
+    0: [(1, 4), (7, 8)],
+    1: [(0, 4), (2, 8), (7, 11)],
+    2: [(1, 8), (3, 7), (8, 2), (5, 4)],
+    3: [(2, 7), (4, 9), (5, 14)],
+    4: [(3, 9), (5, 10)],
+    5: [(4, 10), (3, 14), (2, 4), (6, 2)],
+    6: [(5, 2), (7, 1), (8, 6)],
+    7: [(0, 8), (1, 11), (8, 7), (6, 1)],
+    8: [(2, 2), (7, 7), (6, 6)]
+}
+
+source = 0
+
+sort_dist = dijkstra_algorithms(graph,source)
+
+print(sort_dist)
+
