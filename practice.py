b848a7df14e44f96018edb21caff0284046f8f74
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-01-18 16:00:40 +0200

    update my playground

--- practice.py
+++ practice.py
@@ -295,30 +295,23 @@ example_input = [1, 2, 3, 4, 5, 1]
 sol = Solution().containsDuplicate(example_input)
 print(f"Contains Duplicate: {sol}")
 
-def min_day_cost(days,costs):
+def min_days_costs(days,costs):
     dp = {}
-    travels_days = set(days)
+    days_for_travels = set(days)
 
-    for day in range (1,days[-1]+1):
-
-        if day not in travels_days:
-
-            dp[day] = dp.get(day - 1, 0)
+    for day in range(1, days[-1] +1):
+        if day not in days_for_travels:
+            dp[day] = dp.get(day -1, 0)
         else:
             dp[day] = min(
                 dp.get(day - 1, 0) + costs[0],
-                dp.get(day - 7, 0) + costs[1],
+                dp.get(day - 7, 0 ) + costs[1],
                 dp.get(day - 30, 0) + costs[2]
             )
-
-    return dp[days[-1]]
-                                
-
+    return dp[days[-1]] 
 days = [1, 4, 6, 7, 8, 20]
 costs = [2, 7, 15]
-
-min_day = min_day_cost(days,costs)
+min_day = min_days_costs(days,costs)
 
 print(min_day)
-
 
