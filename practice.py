afc34c30faee0b57483009538cff4daef4534624
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-05 17:18:39 +0200

    update  the maximum activities question

--- practice.py
+++ practice.py
@@ -20,3 +20,29 @@ amount = 41  # Target amount in cents
 list_oin, nums_coin = make_denominations(denominations, amount)
 print(list_oin)
 print(nums_coin)
+
+def maximum_activities(start,end):
+
+    activities = list(zip(start,end))
+    activities.sort(key=lambda x:x[1])
+
+    last_end = activities[0][1]
+    count = 1
+    selected_activities = [0]
+
+    for i in range(1, len(activities)):
+
+        if  activities[i][0]>= last_end:
+            count+=1
+            selected_activities.append(i)
+
+            last_end = activities[i][1]
+
+    return count, selected_activities
+start = [1, 3, 0, 5, 8, 5]
+end = [2, 4, 6, 7, 9, 9]
+
+count,selected = maximum_activities(start,end)
+
+print(count)
+print(selected)
