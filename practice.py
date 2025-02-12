e7296c94f62f30a77df9f5957787616f23bb99b9
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-12 17:35:46 +0200

    reverse string

--- practice.py
+++ practice.py
@@ -402,3 +402,19 @@ target = 9
 indicess =  twosum(numbers, target)
 
 print (indicess)
+
+def reverse_string(s):
+    
+    left, right = 0, len(s)-1
+
+    while left < right:
+        s[left],s[right] = s[right],s[left]
+        left += 1
+        right -= 1
+
+    return s
+    
+s = ["h", "e", "l", "l", "o"] 
+
+tt = reverse_string(s)
+print(tt)
