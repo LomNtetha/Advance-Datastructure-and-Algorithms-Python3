c1e4fc7f1636c721c8721459257e87f5a88e9d8c
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-12 16:54:16 +0200

    adding two  pointer two sum

--- practice.py
+++ practice.py
@@ -382,3 +382,23 @@ k = 2
 
 topk = topkfrequentnum(nums,k)
 print(topk)
+
+def twosum(numbers,target):
+
+    left,right = 0, len(numbers) -1
+
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+        if current_sum == target:
+            return [left +1, right + 1]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+numbers = [2, 7, 11, 15]  # A sorted list of numbers.
+target = 9
+
+indicess =  twosum(numbers, target)
+
+print (indicess)
