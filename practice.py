6538b4e8946fe2fd6e0925c16ef8241b4ca1fbe9
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-14 23:15:58 +0200

    add prefix sum on the playground

--- practice.py
+++ practice.py
@@ -491,3 +491,21 @@ height = [0,1,0,2,1,0,1,3,2,1,2,1]
 
 trappedwater = trap_water(height)
 print(trappedwater)
+
+def sub_array_sum(arr,i,j):
+
+    prefix_sum = [0]* len(arr)
+    prefix_sum[0] = arr[0]
+
+    for k in range(1, len(arr)):
+        prefix_sum[k] = prefix_sum[k - 1] + arr[k]
+
+    if i == 0:
+        return prefix_sum[j]
+    else:
+        return prefix_sum[j] - prefix_sum[i-1]
+arr = [2, 4, 1, 6, 3]
+i, j = 1, 3
+
+prefixsum = sub_array_sum(arr,i,j)
+print(f"The total Prfix form {i} to {j}: {prefixsum}")
