89980a091f0f8c603503527c46d9f931d4d4ddf0
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-20 15:55:21 +0200

    update the playground

--- practice.py
+++ practice.py
@@ -54,3 +54,34 @@ capacity = 50
 t = kanapsack_fractions(weights,values,capacity)
 
 print(t)
+
+def max_platforms_needed(arrival,departure):
+
+    arrival = [time.zfill(5) for time in arrival]
+    departure = [time.zfill(5) for time in departure]
+
+    arrival.sort()
+    departure.sort()
+
+    i,j = 0,0
+    platform_needed = 0
+    max_platform = 0
+    n = len(arrival)
+
+    while i < n and j < n:
+        if arrival[i] < departure[j]:
+            platform_needed +=1
+            max_platform = max(max_platform, platform_needed)
+            i+=1
+
+        else:
+            platform_needed -= 1
+            j+=1
+    return max_platform
+
+arrival = ["9:00", "9:40", "9:50", "11:00", "15:00", "18:00"]
+departure = ["9:10", "12:00", "11:20", "11:30", "19:00", "20:00"]
+
+platform = max_platforms_needed(arrival,departure)
+
+print(platform)
