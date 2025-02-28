e976afb22df30fd75246cbfbceb52da5941296a0
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-02-28 06:05:14 +0200

    adding the longest  cusicutive strike on the practice

--- practice.py
+++ practice.py
@@ -344,3 +344,30 @@ strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
 ana = group_anagrams(strs)
 print(ana)
+
+
+def the_longest_strike(nums):
+
+    num_set = set(nums)
+    longest_strike = 0
+
+    for num in num_set:
+
+        if num - 1 not in num_set:
+            current_num = num
+            current_strike = 1
+        
+        while current_num + 1 in num_set:
+            current_strike += 1
+            current_num += 1
+
+            longest_strike = max(longest_strike, current_strike)
+
+    return longest_strike
+
+
+nums = [100, 4, 200, 1, 3, 2]
+
+longeststrke = the_longest_strike(nums)
+
+print(longeststrke)
