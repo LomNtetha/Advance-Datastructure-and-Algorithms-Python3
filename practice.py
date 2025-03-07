a41d92fdb378b77aa74beffb22a3ca192ff3be12
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-03-07 17:21:51 +0200

    max water that can be trappped

--- practice.py
+++ practice.py
@@ -617,3 +617,36 @@ height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 maximumarea = maxarea(height)
 
 print(maximumarea)
+
+
+def max_trapped_watter(height):
+
+    left, right = 0, len(height) - 1
+
+    left_max, right_max = height[left], height[right]
+
+    water_trapped = 0
+    while left < right:
+
+        if left_max < right_max:
+            left += 1
+            left_max = max(left_max, height[left])
+
+            water_trapped += left_max - height[left]
+
+        else:
+            right -= 1
+
+            right_max = max(right_max, height[right])
+
+            water_trapped += right_max - height[right]
+
+    return water_trapped
+
+
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+
+max_watertrapped = max_trapped_watter(height)
+
+print(max_watertrapped)
