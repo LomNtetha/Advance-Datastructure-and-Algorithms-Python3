f48ea2b3672ed6d02eef1c3bbae2e5ebeae472c1
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-03-07 15:34:16 +0200

    reverse strings

--- practice.py
+++ practice.py
@@ -544,4 +544,20 @@ taget = adding_numbers_target(nums, target)
 
 print(taget)
 
+def reverse_string_two_pointer(s):
+
+    left,right = 0, len(s)-1
+
+    while left < right:
+        s[left],s[right] = s[right],s[left]
+        left+=1
+        right-=1
+    return s
+
+
+
+s = ["h", "e", "l", "l", "o"] 
+revse = reverse_string_two_pointer(s)
+print(revse)
+
 
