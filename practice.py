0a9ab43c7a9fe249e0dce4743b325d480670e504
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-03-14 13:57:15 +0200

    add activities on the playground

--- practice.py
+++ practice.py
@@ -25,3 +25,31 @@ total_denominations, used_coin = denominations_coin(denominations, amount)
 print(total_denominations)
 
 print(used_coin)
+
+
+def max_activities(start, end):
+   
+   activities = list(zip(start,end))
+
+   activities.sort(key=lambda x:x[1])
+
+   last_end = activities[0][1]
+   count = 1
+   select_activities = [0]
+
+   for i in range(1, len(activities)):
+      if activities[i][0] >= last_end:
+         count += 1
+         select_activities.append(i)
+         last_end = activities[i][1]
+   return count, select_activities
+
+start = [1, 3, 0, 5, 8, 5]
+end = [2, 4, 6, 7, 9, 9]
+
+
+num_activities, select_activity = max_activities(start,end)
+
+print(f"Number of activities {num_activities}")
+
+print(f"selected activities {select_activity}")
