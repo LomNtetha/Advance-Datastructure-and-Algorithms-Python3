b67a27cae9da9304ba135ebfd6cb5d96876c28bd
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-03-19 07:17:30 +0200

    longest subsquence dp

--- practice.py
+++ practice.py
@@ -377,3 +377,21 @@ amount = 11
 
 dp_results = dp_denominations(coins,amount)
 print(dp_results)
+
+
+def longest_increasing_subsquence(nums):
+
+   dp = [1] * len(nums)
+
+   for i in range(len(nums)):
+      for j in range(i):
+         if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j ]+ 1)
+
+   return max(dp)
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+
+res = longest_increasing_subsquence(nums)
+
+print(res)
