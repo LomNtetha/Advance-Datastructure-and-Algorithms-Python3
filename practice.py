cbfdf63cff13b88fa9e23c211000f75f9837e382
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-04-11 05:16:05 +0200

    adding combination 2

--- practice.py
+++ practice.py
@@ -252,3 +252,35 @@ target = 7
 combination_value = combinationsum(candidates,target)
 
 print(combination_value)
+
+def combinationsum2(candidates,target):
+
+    result = []
+    candidates.sort()
+
+    def backtrack(start,current,remaining):
+        if remaining == 0:
+            result.append(current[:])
+            return
+        
+        if remaining < 0:
+            return
+        
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+
+            current.append(candidates[i])
+
+            backtrack(i+1,current, remaining - candidates[i])
+            current.pop()
+
+    backtrack(0,[],target)
+    return  result
+
+candidates = [10, 1, 2, 7, 6, 1, 5]
+target = 8
+
+value_combination2 = combinationsum2(candidates,target)
+
+print(value_combination2)
