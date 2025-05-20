4b8a5447e7e60cd770b728049bc077673f4914ef
Lumkile Ntetha <ntethalumkile@gmail.com>
2025-05-20 16:01:40 +0200

    add isPalindrome

--- practice.py
+++ practice.py
@@ -20,6 +20,30 @@ target = 9
 two_pointer = add_two_numbers(nums,target)
 
 print(two_pointer)
+
+def isPalindrome(s):
+
+    left,right =0,len(s)-1
+
+    while left < right:
+
+        while left < right and not s[left].isalnum():
+            left+=1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+        
+    return True
+
+s = "A man, a plan, a canal: Panama"
+
+check_palindrome = isPalindrome(s)
+
+print(check_palindrome)
     
 
     
