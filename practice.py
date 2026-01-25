4c73ad673743d52f76f078ba394a36cf6c2f8a7c
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-01-25 22:48:05 +0200

    max are using two pointer technique

--- practice.py
+++ practice.py
@@ -583,3 +583,30 @@ s = "A man, a plan, a canal: Panama"
 pal = isPalindrome(s)
 
 print(pal)
+
+def maxarea(height):
+
+    left,right = 0, len(height)-1
+
+    max_area = 0
+
+    while left < right:
+
+        width = right - left
+
+        h = min(height[left],height[right])
+
+        max_area = max(max_area, h * width)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+area = maxarea(height)
+
+print(area)
