d3d5e4ea1de358f30571c1c87ac260c97e0f252c
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-01-25 17:41:19 +0200

    combination sum backtrack

--- practice.py
+++ practice.py
@@ -487,3 +487,37 @@ p = permutations(nums)
 
 print(p)
 
+
+def comibination_sum(candidates,target):
+
+    result  = []
+
+    def backtrack(start,current,remaining):
+
+        if remaining == 0:
+            result.append(current[:])
+            return
+        if remaining < 0:
+            return
+        
+        for i in range(start,len(candidates)):
+
+            current.append(candidates[i])
+
+            backtrack(i,current, remaining - candidates[i])
+
+            current.pop()
+
+    backtrack(0,[],target)
+
+    return result
+
+
+
+candidates = [2, 3, 6, 7]
+target = 7
+
+comb = comibination_sum(candidates,target)
+
+print(comb)
+
