dfcbf66ce608e7838fe5a8ca17f15cf8b227110a
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-01-25 21:53:20 +0200

    check if the sentence is palindrome

--- practice.py
+++ practice.py
@@ -562,3 +562,24 @@ def reverstring(s):
 s = ["h", "e", "l", "l", "o"] 
 reverstring(s)
 print(s)
+
+def isPalindrome(s):
+    left,right = 0, len(s)-1
+
+    while left <right:
+        while left < right and not s[left].isalnum():
+            left +=1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        if s[left].lower()!= s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
+        
+s = "A man, a plan, a canal: Panama"
+
+pal = isPalindrome(s)
+
+print(pal)
