3e882af3d21c43f2498b23e56a42ac1e7812e07c
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-02-12 18:23:59 +0200

    add length of longest substring

--- string_practice.py
+++ string_practice.py
@@ -140,3 +140,26 @@ sentences = [
 ]
 
 print(group_word_by_sentence(sentences))
+
+def lenght_longest_substring(s):
+
+    seen = set()
+    left = 0
+    max_len = 0
+
+    for right, char in enumerate(s):
+
+        while char in seen:
+            seen.remove(s[left])
+            left += 1
+
+        seen.add(char)
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+s = "abcabcbb"
+
+
+print(lenght_longest_substring(s))
