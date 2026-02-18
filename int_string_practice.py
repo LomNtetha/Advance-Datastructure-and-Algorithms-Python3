31be628ae1a0e43da7bfaaf5c87ed75658007b65
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-02-18 18:40:39 +0200

    top 5 charachers and words

--- int_string_practice.py
+++ int_string_practice.py
@@ -38,9 +38,35 @@ def most_freq_words(text):
     most_freq_words= count.most_common()
 
     for word,freq in most_freq_words:
-        result.append(word)
+        result.append((word,freq))
 
     return result
 text = "apple banana apple apple orange banana"
 
 print(most_freq_words(text))
+import re
+
+from collections import Counter
+def top_5_words_character(text):
+
+    words = re.findall(r'\b\w+\b',text.lower())
+    count = Counter(words)
+    top_5_words = count.most_common(5)
+
+    letters = [char for char in text.lower() if char.isalpha()]
+    count_char = Counter(letters)
+    top_5_char = count_char.most_common(5)
+
+    print("Top 5 words")
+    for word,freq in top_5_words:
+        print(f"{word}:{freq}")
+     
+    print("\nTop 5 characters")
+    for char,fr in top_5_char:
+
+        print(f"{char}:{fr}")
+
+
+text = "This is this a simple sentence example. This sentence is simple."
+
+top_5_words_character(text)
