739b6fe4ba85c1c5b67aa553bcd7413f98bf63c7
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-03-11 18:15:15 +0200

    basic practice

--- string_practice.py
+++ string_practice.py
@@ -19,3 +19,33 @@ def most_freq_words(paragraph):
 paragraph = "Hello world. Hello everyone. World is beautiful."
 
 print(most_freq_words(paragraph))
+
+
+def ma_number(arr):
+
+    max_num = min(arr)
+
+    return max_num
+
+
+arr = [10, 5, 8]
+
+print(ma_number(arr))
+
+def even_odd(numbers):
+
+    even = 0
+    odd = 0
+    # result = []
+
+    for num in numbers:
+        if num % 2 == 0:
+            even += 1
+            # result.append(even)
+        elif num % 2 == 1:
+            odd += 1
+            # result.append(odd)
+    return odd,even
+numbers = [1,2,3,4]
+
+print(even_odd(numbers))
