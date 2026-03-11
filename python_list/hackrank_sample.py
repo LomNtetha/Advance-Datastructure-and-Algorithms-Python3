2685e5c9bccaa0b5b0af0e2af3a9ab4fe68c6846
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-03-11 16:50:45 +0200

    hackrank sample coding

--- python_list/hackrank_sample.py
+++ python_list/hackrank_sample.py
@@ -931,3 +931,53 @@ def swapFirstLast(arr):
     return arr
 
 print(swapFirstLast([1, 2, 3, 4]))  # Output: [4, 2, 3, 1]
+
+"""
+Problem
+
+Given an integer n, return the numbers from 1 to n following these rules:
+
+If the number is divisible by 3, print "Fizz".
+
+If the number is divisible by 5, print "Buzz".
+
+If the number is divisible by both 3 and 5, print "FizzBuzz".
+
+Otherwise, print the number itself.
+
+Example
+
+For n = 15
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+"""
+
+class Solution:
+    def fizzBuzz(self, n: int):
+
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+
+solution = Solution()
+solution.fizzBuzz(15)
