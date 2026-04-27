853bae41716bb9637e02b54751bc1a2b6d5ef072
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-04-27 12:40:05 +0200

    two pointer two sum

--- practice.py
+++ practice.py
@@ -664,3 +664,29 @@ t = "add"
 
 print(isIsomorphic(s,t))
 
+def twoSum(nums,target):
+
+    left,right = 0,len(nums)-1
+
+    current_sum = 0
+    result = []
+
+    while left < right:
+
+        current_sum = nums[left] + nums[right]
+
+        if current_sum == target:
+            return [left, right]
+        
+
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums,target))
+
