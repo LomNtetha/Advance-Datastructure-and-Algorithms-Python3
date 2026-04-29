f5df51e44b7b239a13ce867012b653cb7c573cee
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-04-29 11:31:56 +0200

    backtracking combination

--- practice.py
+++ practice.py
@@ -883,3 +883,37 @@ def permutation(nums):
 nums = [1, 2, 3]
 
 print(permutation(nums))
+
+def  combination(candidates,target):
+
+    result = []
+
+    def backtrack(start,current,remaining):
+
+        if remaining == 0:
+            result.append(current[:])
+            return
+        if remaining < 0:
+            return
+        
+
+
+        for i in range(start,len(candidates)):
+
+
+            current.append(candidates[i])
+
+            backtrack(i,current, remaining - candidates[i])
+
+            current.pop()
+
+    backtrack(0,[],target)
+
+    return result
+
+    
+
+candidates = [2, 3, 6, 7]
+target = 7
+
+print(combination(candidates,target))
