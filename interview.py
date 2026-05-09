d5f652bd6070afd13b02cc9e7786443db51ab1af
Lumkile Ntetha <ntethalumkile@gmail.com>
2026-05-09 10:23:05 +0200

    most appearing character

--- interview.py
+++ interview.py
@@ -170,3 +170,34 @@ s = "ccaabbb"
 k = 2
 
 print(longest_k_distinct(s,k))
+
+from collections import Counter
+
+def longest_repeating_charater(s,k):
+
+    max_freq = 0
+    left = 0
+    res = 0
+    count = Counter()
+
+    for right in range(len(s)):
+        count[s[right]] += 1
+
+        max_freq = max(max_freq, count[s[right]])
+
+        while (right - left + 1) - max_freq > k:
+
+            count[s[left]] -= 1
+            left += 1
+
+        curr_window = right -left + 1
+
+        res = max(res,curr_window)
+
+    return res
+
+
+s = "AABABBA"
+k = 1
+
+print(longest_repeating_charater(s,k))
